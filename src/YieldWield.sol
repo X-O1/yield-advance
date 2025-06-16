d16da28bb1b03b7c80da91b378085fdc695882c3
X-O1 <series-X-01@protonmail.com>
2025-06-16 15:24:59 -0700

    Updated

--- src/YieldWield.sol
+++ src/YieldWield.sol
@@ -1,4 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
 
+import "./YieldWieldErrors.sol";
+import {IERC20} from "@openzeppelin/ERC20/IERC20.sol";
+
 contract YieldWield {}
--- test/unit/YieldWieldTest.t.sol
+++ test/unit/YieldWieldTest.t.sol
@@ -3,8 +3,14 @@ pragma solidity ^0.8.30;
 
 import {Test, console} from "forge-std/Test.sol";
 import {YieldWield} from "../../src/YieldWield.sol";
+import {IERC20} from "@openzeppelin/ERC20/IERC20.sol";
+import {IPool} from "@aave-v3-core/interfaces/IPool.sol";
 
-contract YieldWieldTest is Test {
+/**
+ * @title Test for YieldWield.sol on the BASE Mainnet
+ * @notice All addresses are for Base Mainnet
+ */
+contract YieldAdaptertTest is Test {
     YieldWield yieldWield;
 
     function setUp() external {
